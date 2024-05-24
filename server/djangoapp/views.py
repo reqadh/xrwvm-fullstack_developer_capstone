0099303f3f3aece7821ae88cfc0395edc6786524
reqadh <128897272+reqadh@users.noreply.github.com>
2024-05-24 17:27:18 +0000

    format 5

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -70,7 +70,7 @@ def registration(request):
         # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
-    except Exception :
+    except Exception:
         logger.debug("{} is new user".format(username))
     # If it is a new user
     if not username_exist:
@@ -99,7 +99,8 @@ def get_cars(request):
     car_models = CarModel.objects.select_related("car_make")
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append({"CarModel": car_model.name,
+                    "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
 
 
@@ -164,7 +165,7 @@ def add_review(request):
         try:
             post_review(data)
             return JsonResponse({"status": 200})
-        except Exception :
+        except Exception:
             return JsonResponse({"status": 401, "message": "Error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})

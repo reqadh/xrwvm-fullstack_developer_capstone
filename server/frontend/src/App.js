999c030fc3f1e87a2e6ea6051bcf8c8525986830
reqadh <noraaksa@icloud.com>
2024-05-25 16:47:22 -0400

    m

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -2,7 +2,9 @@ import LoginPanel from "./components/Login/Login"
 import Register from "./components/Register/Register"
 import Dealers from './components/Dealers/Dealers';
 import Dealer from './components/Dealers/Dealer';
+import PostReview from "./components/Dealers/PostReview"
 import { Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       <Route path="/register" element={<Register />} />
       <Route path="/dealers" element={<Dealers/>} />
       <Route path="/dealer" element={<Dealer/>} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
